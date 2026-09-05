cbfd72775b054e90ea70473e0f8e314d84f7843f
mahmoud311mha-del <mahmoud311mha@gmail.com>
2026-09-06 00:53:33 +0300

    Refactor build.gradle.kts for formatting and Java version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,26 +3,40 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
+
 android {
-    namespace="com.musicplayerab"
-    compileSdk=35
+    namespace = "com.musicplayerab"
+    compileSdk = 35
+
     defaultConfig {
-        applicationId="com.musicplayerab"
-        minSdk=26
-        targetSdk=35
-        versionCode=1
-        versionName="1.0"
+        applicationId = "com.musicplayerab"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
+
+kotlin {
+    jvmToolchain(17)
+}
+
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material3:material3:1.3.1")
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
+
     implementation("androidx.media3:media3-exoplayer:1.5.1")
     implementation("androidx.media3:media3-session:1.5.1")
 }
